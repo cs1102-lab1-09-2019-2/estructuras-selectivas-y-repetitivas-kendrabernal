f734c7518d1b841cb6b23f88823a34d87f6e7bc9
kendrabernal <54277329+kendrabernal@users.noreply.github.com>
2019-09-02 15:52:31 -0500

    Update ejercicio2.cpp

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -1,6 +1,17 @@
+#include <iostream>
 
-
+using namespace std;
 int main() {
-
-    return 0;
+  int n;
+  cin>>n;
+  for (int i=0; i<n;i++){
+    for (int j=0; j<n;j++){
+      if (i==0 or i==n-1 or j==0 or j==n-1 or i==j) 
+        cout<<"*";
+      else
+        cout<<" ";
+    }
+    cout<<endl;
+  }
+  
 }
