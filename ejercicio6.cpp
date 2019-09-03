d240670c90135c9c503af5898a63cf033835252e
kendrabernal <54277329+kendrabernal@users.noreply.github.com>
2019-09-03 10:31:59 -0500

    Update ejercicio6.cpp

--- ejercicio6.cpp
+++ ejercicio6.cpp
@@ -1,6 +1,31 @@
+#include <iostream>
 
-
+using namespace std;
 int main() {
+  int N;
+  bool intercalado=false;
+  cin>>N;
+  for (int i=0; i<N;i++){
+    for (int j=0; j<N;j++){ 
+      
+      if(!intercalado){
+        cout<<"@";
+        
+        
+      }else
+        cout<<"#";
+        intercalado=!intercalado;
+       
+      
+    }
+    if (N%2==0){
+      cout<<endl;
+      intercalado=!intercalado;
+    }else
+      cout<<endl;
 
-    return 0;
+    
+    
+  }
+  
 }
